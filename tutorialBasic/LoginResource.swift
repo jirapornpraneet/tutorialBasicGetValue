b51b326a4271588f1279399bdfd76bfe7b06f053
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-10-10 11:48:37 +0700

    เพิ่มหน้าและการทำงาน ของหน้า LoginViewController

---
+++ tutorialBasic/LoginResource.swift
@@ -0,0 +1,14 @@
+//
+//  LoginResource.swift
+//  tutorialBasic
+//
+//  Created by Jiraporn Praneet on 1/10/2561 BE.
+//  Copyright © 2561 InformatixPlusCompanyLimited. All rights reserved.
+//
+
+import UIKit
+import EVReflection
+
+class LoginResource: EVObject {
+    var accessToken: String?
+}
--- tutorialBasic/ViewController.swift
+++ tutorialBasic/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Alamofire
+import EVReflection
 
 class ViewController: UIViewController {
 
+    var loginResource = LoginResource()
+    var accessToken: String?
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        let url = "https://api.skylpr.com/index.php/api/v1/auth/verify/password"
+        let parameters: [String: Any] = ["username": usernameTextField.text!, "password": passwordTextField.text!]
+        //        let headers = ["Authorization": "Bearer " + accessToken!]
+        
+        Alamofire.request(url, method: .post, parameters: parameters).responseObject { (response: DataResponse<LoginResource>) in
+            switch response.result {
+            case .success:
+                let resource = response.value
+                self.accessToken = resource?.accessToken
+                self.loginResource.accessToken = resource?.accessToken
+                print("loginResource\(self.loginResource)")
+            case .failure(let error):
+                print("ERROR with '\(error)")
+            }
+        }
     }
-
-
 }
 
